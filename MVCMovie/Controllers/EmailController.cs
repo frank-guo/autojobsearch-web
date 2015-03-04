57807ed8e84d2387bb2dfb7d8e40b37ab033e822
frank-guo <frank_guoguo@hotmail.com>
2015-03-03 22:32:16 -0800

    Enable the emailController to send out an email with all job titles on the first page

--- MVCMovie/Controllers/EmailController.cs
+++ MVCMovie/Controllers/EmailController.cs
@@ -36,7 +36,7 @@ namespace MVCMovie.Controllers
             //Timer checkForTime = new Timer(interval);
             //checkForTime.Elapsed += new ElapsedEventHandler((sender, e) => 
             //    checkForTime_Elapsed(sender, e, address, password));
-           // checkForTime.Enabled = true;
+            // checkForTime.Enabled = true;
 
             sendEmail(address, password);
 
@@ -75,7 +75,7 @@ namespace MVCMovie.Controllers
             {
                 return false;
             }
-            
+
             DateTime dateTime = DateTime.Now;
             string today = dateTime.ToString(dateFormat);
 
@@ -98,9 +98,114 @@ namespace MVCMovie.Controllers
             return false;
         }
 
-        private void sendEmail(string address, string password){
+        private void sendEmail(string address, string password)
+        {
 
+            EmailProcessor emailProcessor = new EmailProcessor();
+
+            // Create the thread object, passing in the Alpha.Beta method
+            // via a ThreadStart delegate. This does not start the thread.
+            //Thread oThread = new Thread(new ThreadStart(oAlpha.Beta));
+
+            var t = new Thread(() => emailProcessor.sendAllJobs(address, password));
+
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+        }
+
+    }
+
+
+    //This class has to be run in a separate thread because of using WebBrowser
+    public class EmailProcessor : Form
+    {
+        private string sentDate = "C:/Users/Frank/Documents/Visual Studio 2013/Projects/MVCMovie/sentDate.txt";
+        private string dateFormat = "MM/dd/yy";
+
+        // This method that will be called when the thread is started
+        public void sendAllJobs(string address, string password)
+        {
             RecruitingSiteDBContext db = new RecruitingSiteDBContext();
+            var qry = from s in db.RecruitingSites
+                      select s;
+            qry = qry.Where(s => s.ID == 1);
+            RecruitingSite site = qry.FirstOrDefault();
+
+            WebClient client = new WebClient();
+            string html = client.DownloadString(site.url);
+            WebBrowser browser = new WebBrowser();
+            browser.ScriptErrorsSuppressed = true;
+            browser.Url = new Uri("about:blank"); ;
+            HtmlDocument document = browser.Document.OpenNew(true);
+            document.Write(html);
+
+            string body = "";
+
+            List<PathNode> JobPath = new List<PathNode>();
+            foreach (PathNode p in site.JobPath)
+            {
+                JobPath.Add(p);
+            }
+
+            HtmlElement parent1 = document.GetElementsByTagName("html")[0];
+            HtmlElement node1; 
+
+            int i;
+            HtmlElementCollection children;
+
+            for (i = JobPath.Count - 2; i >= 0; i--)
+            {
+                if (!(JobPath.ElementAt<PathNode>(i).hasCommonParent))
+                {
+                    //Get the child of parent1 at the position
+                    children = parent1.Children;
+                    node1 = children[JobPath.ElementAt<PathNode>(i).position];
+
+                    parent1 = node1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            //parent1 is currently at th level of the common ancestor
+            //i is currently at the level one lower than the common ancestor
+            //Get all the job title nodes
+            var startIdx = --i;
+            children = parent1.Children;
+
+            foreach (HtmlElement child in children)
+            {
+                node1 = child;
+                try
+                {
+                    for (; i >= 0; i--)
+                    {
+                        node1 = node1.Children[JobPath.ElementAt<PathNode>(i).position];
+                    }
+                }
+                //Some of branches of the common ancestor of Node1 and Node2 might not be the job title,
+                //e.g. The leaf of the first branch on T-Net is a img tag.
+                //Thus, this case may throw out an out-of-bound excpetion
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                i = startIdx;
+                if (node1 != null)
+                {
+                    body += node1.InnerHtml + "\n";
+                }
+            }
+
+            sendEmail(address, password, body);
+
+        }
+
+        private void sendEmail(string address, string password, string body)
+        {
 
             SmtpClient client = new SmtpClient();
             client.Port = 587;
@@ -115,13 +220,7 @@ namespace MVCMovie.Controllers
             mm.From = new MailAddress(address);
             mm.To.Add(new MailAddress(address));
             mm.Subject = "T-Net Jobs - Today";
-
-            var qry = from s in db.RecruitingSites
-                      select s;
-            qry = qry.Where(s => s.ID == 1);
-            RecruitingSite site = qry.FirstOrDefault();
-            //mm.Body = createBody(site);
-            createBody(site);
+            mm.Body = body;
 
             //mm.BodyEncoding = UTF8Encoding.UTF8;
             mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
@@ -156,164 +255,5 @@ namespace MVCMovie.Controllers
             }
         }
 
-        private void createBody(RecruitingSite site)
-        {
-
-            Alpha oAlpha = new Alpha();
-
-            // Create the thread object, passing in the Alpha.Beta method
-            // via a ThreadStart delegate. This does not start the thread.
-            //Thread oThread = new Thread(new ThreadStart(oAlpha.Beta));
-
-            var t = new Thread( ()=> oAlpha.createBody1(site));
-
-            t.SetApartmentState(ApartmentState.STA);
-            t.Start();
-        }
-
-        private string createBody1(RecruitingSite site)
-        {
-            //HtmlWeb htmlWeb = new HtmlWeb();
-            //HtmlDocument document = htmlWeb.Load(site.url);
-
-            WebClient client = new WebClient();
-            string html = client.DownloadString(site.url);
-            WebBrowser browser = new WebBrowser();
-            browser.ScriptErrorsSuppressed = true;
-            HtmlDocument document = browser.Document.OpenNew(true);
-
-            string body = "";
-
-            List<PathNode> JobPath = new List<PathNode>();
-            foreach (PathNode p in site.JobPath)
-            {
-                JobPath.Add(p);
-            }
-
-            HtmlElement parent1 = document.GetElementById("html");
-            HtmlElement node1;  // = parent1.Children[JobPath.ElementAt<PathNode>(length-1).position];
-
-            int i;
-            HtmlElementCollection children;
-
-            for (i = JobPath.Count - 2; i>=0 ; i--) 
-            {
-                if ( !(JobPath.ElementAt<PathNode>(i).hasCommonParent) )
-                {
-                    //Get the child of parent1 at the position
-                    children = parent1.Children;
-                    node1 = children[JobPath.ElementAt<PathNode>(i).position];
-                    
-                    /*int j = JobPath.ElementAt<PathNode>(i).position;
-                    node1 = parent1.FirstChild;
-                    while (j > 0 || (node1.NodeType != HtmlNodeType.Element))
-                    {
-                        if ((node1.NodeType == HtmlNodeType.Element))
-                        {
-                            j--;
-                        }
-                        node1 = node1.NextSibling;
-                    }
-                    */
-                    parent1 = node1;
-                }
-                else {
-                    break;
-                }
-            }
-
-            //parent1 is currently at th level of the common ancestor
-            //i is currently at the level one lower than the common ancestor
-            //Get all the job title nodes
-            var startIdx = --i;
-            children = parent1.Children;
-            foreach(HtmlElement child in children){
-                node1 = child;
-                for (; i >= 0; i--) {
-                    node1 = node1.Children[JobPath.ElementAt<PathNode>(i).position];
-                }
-                i = startIdx;
-                body  += node1.InnerHtml;
-            }
-
-            return body;
-        }
     }
-
-    public class Alpha : Form
-    {
-
-        // This method that will be called when the thread is started
-
-        public void createBody1(RecruitingSite site)
-        {
-            //HtmlWeb htmlWeb = new HtmlWeb();
-            //HtmlDocument document = htmlWeb.Load(site.url);
-
-            WebClient client = new WebClient();
-            string html = client.DownloadString(site.url);
-            WebBrowser browser = new WebBrowser();
-            browser.ScriptErrorsSuppressed = true;
-            browser.Url = new Uri("about:blank");;
-            HtmlDocument document = browser.Document.OpenNew(true);
-            document.Write(html);
-
-            string body = "";
-
-            List<PathNode> JobPath = new List<PathNode>();
-            foreach (PathNode p in site.JobPath)
-            {
-                JobPath.Add(p);
-            }
-
-            HtmlElement parent1 = document.GetElementsByTagName("html")[0];
-            HtmlElement node1;  // = parent1.Children[JobPath.ElementAt<PathNode>(length-1).position];
-
-            int i;
-            HtmlElementCollection children;
-
-            for (i = JobPath.Count - 2; i>=0 ; i--) 
-            {
-                if ( !(JobPath.ElementAt<PathNode>(i).hasCommonParent) )
-                {
-                    //Get the child of parent1 at the position
-                    children = parent1.Children;
-                    node1 = children[JobPath.ElementAt<PathNode>(i).position];
-                    
-                    /*int j = JobPath.ElementAt<PathNode>(i).position;
-                    node1 = parent1.FirstChild;
-                    while (j > 0 || (node1.NodeType != HtmlNodeType.Element))
-                    {
-                        if ((node1.NodeType == HtmlNodeType.Element))
-                        {
-                            j--;
-                        }
-                        node1 = node1.NextSibling;
-                    }
-                    */
-                    parent1 = node1;
-                }
-                else {
-                    break;
-                }
-            }
-
-            //parent1 is currently at th level of the common ancestor
-            //i is currently at the level one lower than the common ancestor
-            //Get all the job title nodes
-            var startIdx = --i;
-            children = parent1.Children;
-            foreach(HtmlElement child in children){
-                node1 = child;
-                for (; i >= 0; i--) {
-                    node1 = node1.Children[JobPath.ElementAt<PathNode>(i).position];
-                }
-                i = startIdx;
-                body  += node1.InnerHtml;
-            }
-
-            //return body;
-        }
-    }
-
 }
