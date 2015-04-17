5752b690311a60ef295b1b7927f6b4609a4a9053
frank-guo <frank_guoguo@hotmail.com>
2015-04-16 23:56:58 -0700

    Missed committing these two new models last time

---
+++ MVCMovie/Models/IframeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMovie.Models
+{
+    public class IframViewModel
+    {
+        public int ID { get; set; }
+        public string webpageUpdate { get; set; }
+    }
+}
---
+++ MVCMovie/Models/Job2Position.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.ComponentModel;
+
+namespace MVCMovie.Models
+{
+    public class Job2Position
+    {
+        public int ID { get; set; }
+        public int position { get; set; }
+    }
+}
