19d7c3f94390ec6b3c446483a6ee48fe2672ddae
frank-guo <frank_guoguo@hotmail.com>
2017-03-13 21:13:15 -0700

    Set up ApplicationDbContext migration

---
+++ MVCMovie/Migrations/ApplicationDbContext/Configuration.cs
@@ -0,0 +1,40 @@
+namespace MVCMovie.Migrations.ApplicationDbContext
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVCMovie.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\ApplicationDbContext";
+            ContextKey = "MVCMovie.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(MVCMovie.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            role.Name = "Regular";
+            roleManager.Create(role);   
+        }
+    }
+}
